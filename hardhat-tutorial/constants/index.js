9aacf0ae3168ffc17af80da50695188804531a81
augustinenwike <augustineudo03@gmail.com>
2022-05-09 22:45:21 +0100

    NFTs Collection

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x12bF1b878077bA5cCFdEED03926798E102b40Ee6";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
