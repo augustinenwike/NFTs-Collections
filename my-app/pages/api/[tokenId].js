6b5fca1a73e0b15fab9f8a4b4bbe921fc7dd0490
augustinenwike <augustineudo03@gmail.com>
2022-05-11 10:26:47 +0100

    img-url

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   const image_url =
-    "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/Cryptodevs/";
+    "https://github.com/augustinenwike/NFTs-Collections/tree/main/my-app/public/Cryptodevs";
   res.status(200).json({
     name: "Crypto Dev #" + tokenId,
     description: "Crypto Dev is a collection of developers in crypto",
